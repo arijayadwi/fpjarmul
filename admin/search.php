bc4da52857ba0cb39dee72e8eef691bc2a32b990
Arya Bawanta I Gusti Ngurah <Arya Bawanta I Gusti Ngurah>
2016-12-19 19:30:16 +0700

    asu

--- admin/search.php
+++ admin/search.php
@@ -247,213 +247,64 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="main-grids">
 				<div class="top-grids">
 					<div class="recommended-info">
-						<h3>Recent Videos</h3>
+						<h3>Search Result</h3>
 					</div>
-					<?php while($row = $result->fetch_assoc()) {
-						$SendJudul = preg_replace('/\s+/', '', $row["Judul"]);
-			         	echo"	
-			         			<div class='col-md-4 resent-grid recommended-grid slider-top-grids'>
-									<div class='resent-grid-img recommended-grid-img'>
-										<a href='insert_views.php?ID=". $row["Indexes"]."&t=0'><img src='Thumbnail/". $row["Thumbnail"].".jpg' alt='' /></a>
-										<div class='time'>
-											<p>". $row["Times"]."</p>
+					<?php 
+						include("connect.php");
+						//if(isset($_POST['submit'])){ 
+							$name=$_POST['name']; 
+							//-query  the database table 
+							$sql="SELECT  * FROM video WHERE Judul LIKE '%" . $name .  "%'"; 
+							//-run  the query against the mysql query function 
+							$result=mysqli_query($conn,$sql); 
+							//-create  while loop and loop through result set 
+							while($row=mysqli_fetch_array($result)){ 
+							    $judul  =$row['Judul']; 
+							    $index=$row['Indexes']; 
+							  //-display the result of the array 
+								echo"	
+								  	<div class='col-md-4 resent-grid recommended-grid slider-top-grids'>
+										<div class='resent-grid-img recommended-grid-img'>
+											<a href='insert_views.php?ID=". $row["Indexes"]."&t=0'><img src='Thumbnail/". $row["Thumbnail"].".jpg' alt='' /></a>
+											<div class='time'>
+												<p>". $row["Times"]."</p>
+											</div>
+											<div class='clck'>
+												<span class='glyphicon glyphicon-time' aria-hidden='true'></span>
+											</div>
 										</div>
-										<div class='clck'>
-											<span class='glyphicon glyphicon-time' aria-hidden='true'></span>
+										<div class='resent-grid-info recommended-grid-info'>
+											<h3><a href='insert_views.php?ID=". $row["Indexes"]."&t=0' class='title title-info'>". $row["Judul"]."</a></h3>
+											<ul>
+												<li><p class='author author-info'><a href='#'' class='author'>". $row["Uploader"]."</a></p></li>
+												<li class='right-list'><p class='views views-info'>". $row["Views"]." views</p></li>
+											</ul>
 										</div>
-									</div>
-									<div class='resent-grid-info recommended-grid-info'>
-										<h3><a href='insert_views.php?ID=". $row["Indexes"]."&t=0' class='title title-info'>". $row["Judul"]."</a></h3>
-										<ul>
-											<li><p class='author author-info'><a href='#'' class='author'>". $row["Uploader"]."</a></p></li>
-											<li class='right-list'><p class='views views-info'>". $row["Views"]." views</p></li>
-										</ul>
-									</div>
-								</div>         																							
-							";
-			     	} ?>
+									</div> "; 
+							};
+						//} else{ 
+						//	echo  "<p>Please enter a search query</p>"; 	 
+					  	//} 
+					?> 
 					<div class="clearfix"> </div>
 				</div>
 				<div class="recommended" id="recommended">
 					<div class="recommended-grids">
 						<div class="recommended-info">
-							<h3>Recommended</h3>
-						<?php while($row1 = $result1->fetch_assoc()) {						
-			         	echo"	
-			         			<div class='col-md-3 resent-grid recommended-grid'>
-									<div class='resent-grid-img recommended-grid-img'>
-										<a href='insert_views.php?ID=". $row1["Indexes"]."&t=0'><img src='Thumbnail/". $row1["Thumbnail"].".jpg' alt='' /></a>
-										<div class='time small-time'>
-											<p>". $row["Times"]."</p>
-										</div>
-										<div class='clck small-clck'>
-											<span class='glyphicon glyphicon-time' aria-hidden='true'></span>
-										</div>
-									</div>
-									<div class='resent-grid-info recommended-grid-info video-info-grid'>
-										<h5><a href='insert_views.php?ID=". $row1["Indexes"]."&t=0' class='title'>". $row1["Judul"]."</a></h5>
-										<ul>
-											<li><p class='author author-info'><a href='#'' class='author'>". $row1["Uploader"]."</a></p></li>
-											<li class='right-list'><p class='views views-info'>". $row1["Views"]." views</p></li>
-										</ul>
-									</div>
-								</div>    																							
-							";
-			     	} ?>
-					</div>					
-				</div>
+						</div>					
+					</div>
 				<div class="recommended">
 					<div class="recommended-grids">
-						<div class="recommended-info">
-							<h3>Sports</h3>
+						
 						</div>
 						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/g.jpg" alt="" /></a>
-								<div class="time small-time">
-									<p>7:30</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Varius sit sed viverra nullam viverra nullam interdum metus</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/g1.jpg" alt="" /></a>
-								<div class="time small-time">
-									<p>9:34</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Nullam interdum viverra nullam metus varius sit sed viverra</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/g2.jpg" alt="" /></a>
-								<div class="time small-time">
-									<p>5:34</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Varius sit sed viverra nullam viverra nullam interdum metus</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/g3.jpg" alt="" /></a>
-								<div class="time small-time">
-									<p>6:55</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Nullam interdum metus viverra nullam varius sit sed viverra</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
+							
 						</div>
 						<div class="clearfix"> </div>
 					</div>
 					<div class="recommended-grids">
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/we2.jpg" alt=""></a>
-								<div class="time small-time">
-									<p>7:30</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Varius sit sed viverra nullam viverra nullam interdum metus</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/we1.jpg" alt=""></a>
-								<div class="time small-time">
-									<p>9:34</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Nullam interdum viverra nullam metus varius sit sed viverra</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/we4.jpg" alt=""></a>
-								<div class="time small-time">
-									<p>5:34</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Varius sit sed viverra nullam viverra nullam interdum metus</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-md-3 resent-grid recommended-grid">
-							<div class="resent-grid-img recommended-grid-img">
-								<a href="single.html"><img src="images/we3.jpg" alt=""></a>
-								<div class="time small-time">
-									<p>6:55</p>
-								</div>
-								<div class="clck small-clck">
-									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-								</div>
-							</div>
-							<div class="resent-grid-info recommended-grid-info video-info-grid">
-								<h5><a href="single.html" class="title">Nullam interdum metus viverra nullam varius sit sed viverra</a></h5>
-								<ul>
-									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
-									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
-								</ul>
-							</div>
-						</div>
+						
+						
 						<div class="clearfix"> </div>
 					</div>
 				</div>
@@ -492,3 +343,4 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
   </body>
 </html>
+
--- admin/views.php
+++ admin/views.php
@@ -66,8 +66,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         </div>
         <div id="navbar" class="navbar-collapse collapse">
 			<div class="top-search">
-				<form class="navbar-form navbar-right">
-					<input type="text" class="form-control" placeholder="Search...">
+				<form class="navbar-form navbar-right" action="search.php" method="POST">
+					<input type="text" class="form-control" placeholder="Search..." name="name">
 					<input type="submit" value=" ">
 				</form>
 			</div>  
